90f879519a634ab510c6b76634299f6b7c5c5765
Strangahhh <Kongphop.ja.64@ubu.ac.th>
2025-03-02 01:43:24 +0700

    B

--- A/urls.py
+++ A/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import course_list
+from .views import course_list, course_search
 
 urlpatterns = [
     path('courses/', course_list, name='course_list'),
 
-    path('courses_search/', course_list, name='courses_search'),
+    path('courses_search/', course_search, name='courses_search'),
 ]
