e391f0caaddafec3d2c29eb5cd3d2e056610913c
Strangahhh <Kongphop.ja.64@ubu.ac.th>
2025-03-02 01:37:51 +0700

    B

--- A/views.py
+++ A/views.py
@@ -6,3 +6,13 @@ def course_list(request):
     courses = Course.objects.all()
 
     return render(request, "A/course_list.html", {"courses":courses})
+
+def course_list(request):
+    query = request.GET.get('q', '')
+    
+    if query:
+        courses = Course.objects.filter(code__icontains=query)
+    else:
+        courses = Course.objects.all()
+
+    return render(request, "A/course_list.html", {"courses": courses, "query": query})
